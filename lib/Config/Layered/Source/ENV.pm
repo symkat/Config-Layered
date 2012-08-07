8aba5fef75c7f521dc0ff2ee7cbad7e2a5b9a3af
SymKat <symkat@symkat.com>
2012-08-07 14:51:07 -0700

    Now with documentation.

--- lib/Config/Layered/Source/ENV.pm
+++ lib/Config/Layered/Source/ENV.pm
@@ -9,14 +9,106 @@ sub get_config {
 
     my $struct = dclone($self->layered->default);
     my $config = {};
+    $self->args->{prefix} ||= "CONFIG";
 
     for my $key ( keys %{$struct} ) {
-        if ( exists $ENV{ "CONFIG_".uc($key) } ) {
-            $config->{$key} = $ENV{ "CONFIG_" . uc($key) };
-        }
+        $config->{$key} = $ENV{ $self->args->{prefix} . "_" . uc($key) }
+            if exists $ENV{ $self->args->{prefix} . "_" . uc($key) };
+    }
+
+    for my $key ( @{$self->args->{params}} ) {
+        $config->{$key} = $ENV{ $self->args->{prefix} . "_" . uc($key) }
+            if exists $ENV{ $self->args->{prefix} . "_" . uc($key) };
     }
 
     return $config;
 }
 
 1;
+
+=head2 ENV
+
+The ENV source provides configuration through environment variables.
+
+For each top-level key in the default data structure, it checks for the
+environment variable C<CONFIG_$KEY> where $KEY is the name of the key used
+in the default data structure.
+
+=head3 Example
+
+    my $config = Config::Layered->load_config( 
+        default => {
+            foo         => "bar",
+            blee        => "baz",
+            bax         => {
+                chicken => "eggs",
+            }
+        }
+    )
+
+With the above configuratio, the following keys will be checked:
+
+=over 4
+
+=item * CONFIG_FOO
+
+=item * CONFIG_BLEE
+
+=item * CONFIG_BAX
+
+=back
+
+The following would *NOT* be checked:
+
+=over 4
+
+=item * CONFIG_CHICKEN
+
+=back
+
+Given the above default data structure, a command run as 
+C<CONFIG_FOO="Hello World" ./myprogram> would result in a
+C<$config> structure like this:
+
+
+    {
+        foo         => "Hello World",
+        blee        => "baz",
+        bax         => {
+            chicken => "eggs",
+    }
+
+=head3 SOURCE ARGUMENTS
+
+=over 4
+
+=item * params is an array ref of keys to check Default: Keys of the default
+data structure.
+
+=item * prefix is a word prepended to your key that is used to check 
+C<$ENV{$prefix . "_" . uc($key) }>.
+
+Example:
+
+    Config::Layered->load_config(
+        sources => [ 
+            'ENV' => { prefix => "MYAPP", params => [qw( bar blee )] } 
+        ],
+        default => { debug => 0, verbose => 1 },
+    );
+
+The following keys would be checked:
+
+=over 4
+
+=item * MYAPP_BAR
+
+=item * MYAPP_BLEE
+
+=item * MYAPP_DEBUG
+
+=item * MYAPP_VERBOSE
+
+=back
+
+=cut
